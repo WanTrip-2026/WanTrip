41a7804fcc5e26468a7a686a60a6f81077498a95
pbg <102janet@gmail.com>
2025-12-18 11:05:14 +0800

    version 1

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>WanTrip</h1>
+  <p>My first Vue project 🚀</p>
+  <div class="text-3xl font-bold text-red-500">Tailwind 安裝成功！</div>
+</template>
+
+<script setup></script>
+
+<style>
+h1 {
+  color: #42b883;
+}
+</style>
