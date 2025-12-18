6cba63e2fb4ac3faab900a5ede78643082859ae7
Chris <chris27805850@gmail.com>
2025-12-18 14:29:37 +0800

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
+  <Navbar />
   <h1>WanTrip</h1>
-  <p>My first Vue project 🚀</p>
-  <div class="text-3xl font-bold text-red-500">Tailwind 安裝成功！</div>
 </template>
 
-<script setup></script>
+<script setup>
+  import Navbar from './components/layout/Navbar.vue';
+</script>
 
 <style>
-h1 {
-  color: #42b883;
-}
 </style>
