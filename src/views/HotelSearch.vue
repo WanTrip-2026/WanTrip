2d4c77424a103ce624ca42c72bc4e68274a6dd00
Carol <gill20519@gmail.com>
2025-12-20 17:17:21 +0800

    feature/更新住宿搜尋頁面v1

--- src/views/HotelSearch.vue
+++ src/views/HotelSearch.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Navbar from '../components/layout/Navbar.vue'
 import HotelCard from '../components/layout/HotelCard.vue'
+import { ref } from 'vue'
 
 // import Footer from '../components/layout/Footer.vue'
 // import { RouterLink } from 'vue-router'
@@ -9,7 +10,7 @@ const HotelFiltered = [
   { title: '星級', options: ['五星級', '四星級', '三星級'] },
   {
     title: '評價',
-    options: ['好極了:9分以上', '非常好:8分以上', '好:7分以上', '令人愉悅:6分以上'],
+    options: ['好極了: 9分以上', '非常好: 8分以上', '好: 7分以上', '令人愉悅: 6分以上'],
   },
   { title: '住宿類型', options: ['飯店', '旅館', '民宿', '度假村'] },
   { title: '付款政策', options: ['免費取消', '立即付款', '延後付款', '到店付款'] },
@@ -39,6 +40,7 @@ const HotelFiltered = [
     ],
   },
 ]
+const expandedMenus = ref([]) // 儲存哪些 option 已展開
 </script>
 
 <template>
@@ -46,7 +48,7 @@ const HotelFiltered = [
   <main class="max-w-[1200px] mx-auto">
     <!-- search-bar -->
     <section
-      class="max-w-[800px] h-[80px] p-2.5 mt-10 mb-10 mx-auto bg-slate-400 rounded-full flex flex-row items-center gap-5 sticky"
+      class="max-w-[800px] h-[80px] p-2.5 mt-10 mb-10 mx-auto bg-secondary rounded-full flex flex-row items-center gap-5 sticky"
     >
       <div class="h-full w-full">
         <label class="rounded-full"></label>
@@ -81,11 +83,13 @@ const HotelFiltered = [
       </div>
     </section>
     <!-- result-list -->
-    <section class="grid grid-cols-4 gap-[20px] m-[40px]">
-      <aside class="col-span-1 flex flex-col gap-5">
+    <section class="gap-[20px] m-[40px] mx-auto flex">
+      <aside class="flex flex-col gap-5 w-[285px]">
         <!-- 思考：1.太多的項目要怎麼開啟下拉 2.清除按鈕 -->
-        <div class="rounded-[20px]">地圖找房</div>
-        <div class="mx-auto p-[40px] bg-red-200 rounded-[20px]">
+        <div class="rounded-[20px] bg-accent h-40 flex justify-center items-center">
+          地圖找房功能
+        </div>
+        <div class="rounded-[20px] p-10 bg-accent">
           <h3 class="font-bold text-xl mb-[20px]">篩選條件</h3>
           <div class="flex flex-col gap-5">
             <!-- Price Filter -->
@@ -112,20 +116,37 @@ const HotelFiltered = [
               <h4 class="font-medium mb-2 text-base">{{ HotelMenu.title }}</h4>
               <div class="space-y-2">
                 <label
-                  class="flex cursor-pointer text-[14px]"
-                  v-for="option in HotelMenu.options"
+                  class="flex cursor-pointer text-[14px] items-center"
+                  v-for="option in HotelMenu.options.slice(
+                    0,
+                    expandedMenus.includes(HotelMenu.title) ? HotelMenu.options.length : 4,
+                  )"
                   :key="option"
                 >
-                  <input type="checkbox" class="text-nowrap focus:ring-primary" />{{ option }}
+                  <input type="checkbox" class="mr-2 focus:ring-primary" />
+                  {{ option }}
                 </label>
+                <button
+                  v-if="HotelMenu.options.length > 5 && !expandedMenus.includes(HotelMenu.title)"
+                  class="text-black text-sm mt-1"
+                  @click="expandedMenus.push(HotelMenu.title)"
+                >
+                  展開更多選項
+                </button>
               </div>
             </div>
           </div>
         </div>
       </aside>
-      <div class="col-span-3 bg-red-500">
-        <div class="flex flex-row">排序</div>
-        <div class="hotel-card rounded-[20px]"><HotelCard /></div>
+      <div class="flex flex-1 flex-col gap-5">
+        <div class="flex flex-row items-center gap-5 h-[80px]">
+          <button class="rounded-full bg-secondary h-[60px] w-full font-bold">價格高到低</button>
+          <button class="rounded-full bg-secondary h-[60px] w-full font-bold">價格低到高</button>
+          <button class="rounded-full bg-secondary h-[60px] w-full font-bold">熱門高到低</button>
+          <button class="rounded-full bg-secondary h-[60px] w-full font-bold">評價高到低</button>
+          <button class="rounded-full bg-secondary h-[60px] w-full font-bold">排序方式</button>
+        </div>
+        <div class="hotel-card rounded-[20px] w-full"><HotelCard class="w-full" /></div>
       </div>
     </section>
   </main>
