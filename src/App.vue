6e520153c5775579fd2694abb8159f0de3cf0407
Carol <gill20519@gmail.com>
2025-12-23 17:56:24 +0800

    調整app.vue 到最新的版本
    Removed header and paragraph elements, added router view and footer.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <Navbar />
-  <h1>WanTrip</h1>
-  <p>My first Vue project :rocket:</p>
-  <div class="text-3xl font-bold text-red-500">Tailwind 安裝成功！</div>
+  <router-view />
+  <AppFooter />
 </template>
 
+<!--
+網址設定：
+ Home飯店首頁 http://localhost:5173/
+ HotelDetail飯店資訊頁 http://localhost:5173/hotelsdetail/
+ HotelSearch飯店搜尋頁 http://localhost:5173/hotelsearch
+ HotelCompare http://localhost:5173/compare
 
+設定檔案：
+ router/index.ts
+ -->
 
-<style>
-h1 {
-  color: #42b883;
-}
-</style>
+<script setup>
+import Navbar from './components/layout/Navbar.vue'
+import AppFooter from './components/layout/AppFooter.vue'
+</script>
