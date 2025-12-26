6c5a2af6415315e0e1854f3cc725ed7ee65692d9
fiddle <andypeng1416@gmail.com>
2025-12-26 15:43:50 +0800

    HotelHomeView_v1

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,7 @@ import HomePage from '@/views/HomePage.vue'
 import HotelSearch from '@/views/HotelSearch.vue'
 import HotelDetail from '@/views/HotelDetail.vue'
 import HotelCompare from '@/views/HotelCompare.vue'
+import HotelHomeView from '@/views/HotelHomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,6 +29,11 @@ const router = createRouter({
       path: '/compare',
       name: 'hotel-compare',
       component: HotelCompare,
+    },
+    {
+      path: '/hotelhomeview',
+      name: 'hotel-home-view',
+      component: HotelHomeView,
     },
     // 404 頁（選擇性，但很推薦）
     {
