dbe2b12f3f66764130cf1e2d63b78cd58c707873
Chris <chris27805850@gmail.com>
2025-12-29 21:30:45 +0800

    fix/ margin調整

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -1,9 +1,9 @@
 <template>
   <footer class="bg-primary hidden md:block">
-    <div class="max-w-[1240px] mx-auto px-5 pt-10 text-white flex flex-col gap-5 items-center">
-      <div class="w-full flex flex-row justify-between">
+    <div class="max-w-[1200px] mx-auto pt-10 text-white flex flex-col gap-5 items-center">
+      <div class="w-full mx-5 flex flex-row justify-between">
         <div class="grid grid-cols-7 gap-10">
-          <div class="w-[82px]" v-for="footerMenu in footerLinks" :key="footerMenu.title">
+          <div class="w-20" v-for="footerMenu in footerLinks" :key="footerMenu.title">
             <h4 class="font-bold text-lg text-nowrap mb-2">{{ footerMenu.title }}</h4>
             <ul class="opacity-80">
               <li v-for="link in footerMenu.links" :key="link.to">
