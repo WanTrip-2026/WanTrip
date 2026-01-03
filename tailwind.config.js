5e04ced75c875490e41507748bbb7453f5ad8159
Chris <chris27805850@gmail.com>
2026-01-03 11:37:23 +0800

    fix/ 修正缺少的色碼

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        main: '#65475',
+        main: '#365475',
         main_900: '#4A6583',
         main_800: '#5E7691', // Button hover color
         main_700: '#72879E',
