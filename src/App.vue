60752b03bd63311d677a6456fa701d9c0fb1c953
Chris <chris27805850@gmail.com>
2026-01-03 11:27:23 +0800

    style/ 文字字體、顏色與主題顏色設定

--- src/App.vue
+++ src/App.vue
@@ -19,3 +19,15 @@
 import AppNavbar from './components/layout/AppNavbar.vue'
 import AppFooter from './components/layout/AppFooter.vue'
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100..900&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap');
+
+#app {
+  font-family: 'Helvetica', 'Arial', 'SF Pro', 'Noto Sans', 'Noto Sans TC', '微軟正黑體', sans-serif;
+}
+
+body {
+  background-color: #F8FDFF;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,30 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#6D8FA3', // 主色
-        primary_hover: '#365475', // Button hover color
-        secondary: '#E4E4E4', // 輔色
-        secondary_hover: '#C7C7C7',
+        main: '365475',
+        main_900: '#4A6583',
+        main_800: '#5E7691', // Button hover color
+        main_700: '#72879E',
+        main_600: '#8698AC',
+        main_500: '#9AA9BA',
+        main_400: '#AFBBC8',
+        main_300: '#C3CCD6',
+        main_200: '#D7DDE3',
+        main_100: '#EBEEF1',
+
+        primary: '#2F3D4D',
+
+        dark: '#020408',
+        dark_900: '#1B1D21',
+        dark_800: '#353639',
+        dark_700: '#4E4F52',
+        dark_600: '#67686B',
+        dark_500: '#808183',
+        dark_400: '#9A9B9C',
+        dark_300: '#B3B4B5', //第二顆按鈕
+        dark_200: '#CCCDCE', //第二顆按鈕hover
+        dark_100: '#E6E6E6',
         accent: '#93ACAA', // 點綴色
-        page: '#F8FDFF', // Global background
-      },
-      fontFamily: {
-        sans: ['Helvetica', 'Arial', 'SF Pro', 'Noto Sans TC', '微軟正黑體', 'sans-serif'],
       },
     },
   },
