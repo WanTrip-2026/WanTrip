f73628ffc6ecefdb69fc02b536da641835ce1252
pbg <102janet@gmail.com>
2026-01-08 18:24:37 +0900

    feat/加入FontAwesome套件

---
+++ src/fontawesome.ts
@@ -0,0 +1,13 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {
+  faChevronRight,
+  faChevronLeft,
+  faSearch,
+  faUser,
+  faEnvelope,
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faChevronRight, faChevronLeft, faSearch, faUser, faEnvelope)
+
+export { FontAwesomeIcon }
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
-import "./assets/main.css";
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import App from "./App.vue";
-import router from "./router";
-import { useAuthStore } from "./stores/auth";
+import './assets/main.css'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
+import { useAuthStore } from './stores/auth'
+import './assets/main.css'
+import { FontAwesomeIcon } from '@/fontawesome'
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.use(createPinia());
-app.use(router);
+app.use(createPinia())
+app.use(router)
+app.component('FontAwesomeIcon', FontAwesomeIcon)
 
 const authStore = useAuthStore()
 authStore.init()
 
-app.mount("#app");
+app.mount('#app')
