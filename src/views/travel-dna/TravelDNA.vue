2d2e9f1e9222c0709d4e89ea064398d43cbca684
pbg <102janet@gmail.com>
2026-01-26 16:25:33 +0900

    style/調整測驗選項選取效果

--- src/views/travel-dna/TravelDNA.vue
+++ src/views/travel-dna/TravelDNA.vue
@@ -52,10 +52,10 @@
             v-for="(option, index) in currentQuestion.options"
             :key="index"
             @click="handleAnswer(option, index)"
-            class="p-4 md:p-5 rounded-[20px] text-lg md:text-xl font-medium border-2 transition-all duration-200 active:scale-[0.98] shadow-md w-full"
+            class="p-4 md:p-5 rounded-[20px] text-lg md:text-xl font-medium border transition-all duration-200 active:scale-[0.98] shadow-md w-full"
             :class="
               selectedOptionIndex === index
-                ? 'bg-white/90 border-black/30'
+                ? ' bg-primary/80 text-white font-bold scale-105 shadow-xl'
                 : 'bg-[#EEF2F7] border-transparent'
             "
           >
